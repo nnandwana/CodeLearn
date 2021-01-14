8d22f0e13e08c2491ccbfbc4f2eca8ab0036bbbd
Neha Nandwana <neha.nandwana@walmart.com>
2021-01-13 16:23:26 -0800

    FindMaxConsecutiveOnes

---
+++ src/FindMaxConsecutiveOnes.java
@@ -0,0 +1,18 @@
+public class FindMaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int count=0,temp=0;
+
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]==1) {
+                count++;
+            } else {
+                if (count > temp) {
+                    temp = count;
+                }
+                count=0;
+            }
+        }
+        return Math.max(count, temp);
+    }
+}
